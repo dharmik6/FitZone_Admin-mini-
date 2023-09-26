d51baff0bdaa55e97b3a7b0613c73c8675b2709f
Dhruv <dhruvpandav04@gmail.com>
2023-09-26 17:01:02 +0530

    admin login complite and recycler ma data show nathi thata

--- app/src/main/java/com/example/fitzoneadmin/AdminLogin.java
+++ app/src/main/java/com/example/fitzoneadmin/AdminLogin.java
@@ -5,8 +5,15 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class AdminLogin extends AppCompatActivity {
 
@@ -24,8 +31,21 @@ public class AdminLogin extends AppCompatActivity {
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent ilogin = new Intent(AdminLogin.this , AdminHomePage.class);
-                startActivity(ilogin);
+                FirebaseAuth mAuth = FirebaseAuth.getInstance();
+                mAuth.signInWithEmailAndPassword(admin_id.getText().toString(), admin_password.getText().toString())
+                        .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    // Sign-in successful, navigate to the next page
+                                    Intent genderPageIntent = new Intent(AdminLogin.this, AdminHomePage.class);
+                                    startActivity(genderPageIntent);
+                                } else {
+                                    // Sign-in failed, display an error message
+                                    Toast.makeText(AdminLogin.this,"Please enter the correct Email and password", Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
             }
         });
 
