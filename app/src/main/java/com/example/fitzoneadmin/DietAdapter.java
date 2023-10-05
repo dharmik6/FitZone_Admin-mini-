690808bb13c657a2411be196cb89a2a2d59abe9a
dharmik6 <dharmik.kacha.2526@outlook.com>
2023-10-05 10:19:36 +0530

    darshan code

--- app/src/main/java/com/example/fitzoneadmin/DietAdapter.java
+++ app/src/main/java/com/example/fitzoneadmin/DietAdapter.java
@@ -7,15 +7,20 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
+
 import com.bumptech.glide.Glide;
+
+
 import java.util.List;
 
 public class DietAdapter extends RecyclerView.Adapter<DietAdapter.ViewHolder> {
 
     private List<DietItem> dietItems;
     private Context context;
+
 
     public DietAdapter(Context context, List<DietItem> dietItems) {
         this.context = context;
@@ -29,42 +34,37 @@ public class DietAdapter extends RecyclerView.Adapter<DietAdapter.ViewHolder> {
         return new ViewHolder(view);
     }
 
-    @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
         DietItem currentItem = dietItems.get(position);
-        if (currentItem != null) {
-            String dietName = currentItem.getDietName();
-            String dietDescription = currentItem.getDietDescription();
-            String imageUrl = currentItem.getImageUrl();
+        holder.dietNameTextView.setText(currentItem.getDietName());
 
-            // Check if the values are not null before using them
-            if (dietName != null) {
-                holder.dietNameTextView.setText(dietName);
-            } else {
-                holder.dietNameTextView.setText("");
-            }
 
-            // Load the image using Glide
-            if (imageUrl != null && !imageUrl.isEmpty()) {
-                Glide.with(context)
-                        .load(imageUrl)
-                        .into(holder.dietImageView);
-            }
+        // Load user image from Firebase using Glide
+        String imageUrl = currentItem.getDietImageResourceId();
+        Glide.with(context)
+                .load(imageUrl)
+                .into(holder.dietImageView);
 
-            holder.itemView.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    // Pass other properties to the DietData activity if needed
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int position = holder.getAdapterPosition();
+               DietItem item = dietItems.get(position);
+                if (position != RecyclerView.NO_POSITION) {
+                    String dietname = item.getDietName();
+                    String dietdesc = item.getDietDescription();
                     Intent intent = new Intent(context, DietData.class);
-                    intent.putExtra("wname", dietName);
-                    intent.putExtra("wdes", dietDescription);
-                    intent.putExtra("imag", imageUrl);
-                    // Add the FLAG_ACTIVITY_NEW_TASK flag
+                    intent.putExtra("dietname", dietname);
+                    intent.putExtra("dietdesc", dietdesc);
+                    intent.putExtra("dietimage", imageUrl);
                     intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
                     context.startActivity(intent);
+                } else {
+                    // Handle the case where the position is invalid or the view holder is detached.
+                    // You can log an error or display a message to the user.
                 }
-            });
-        }
+            }
+        });
     }
 
     @Override
@@ -74,11 +74,13 @@ public class DietAdapter extends RecyclerView.Adapter<DietAdapter.ViewHolder> {
 
     public class ViewHolder extends RecyclerView.ViewHolder {
         TextView dietNameTextView;
+
         ImageView dietImageView;
 
         public ViewHolder(View itemView) {
             super(itemView);
             dietNameTextView = itemView.findViewById(R.id.dietNameTextView);
+            // Initialize TextView
             dietImageView = itemView.findViewById(R.id.dietImageView);
         }
     }
--- app/src/main/java/com/example/fitzoneadmin/DietData.java
+++ app/src/main/java/com/example/fitzoneadmin/DietData.java
@@ -1,7 +1,5 @@
 package com.example.fitzoneadmin;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -10,19 +8,30 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.bumptech.glide.Glide;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.storage.FirebaseStorage;
-import com.google.firebase.storage.StorageReference;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 
 public class DietData extends AppCompatActivity {
 
     ImageView dietImage ;
     TextView dietName , dietDesc ;
     Button update , delete ;
-    private StorageReference storageReference;
+    FirebaseDatabase database;
     DatabaseReference databaseReference;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -36,13 +45,6 @@ public class DietData extends AppCompatActivity {
         update = findViewById(R.id.btn_Update);
         delete = findViewById(R.id.btn_Delete);
 
-        FirebaseDatabase database = FirebaseDatabase.getInstance();
-        databaseReference = database.getReference("diets");
-        storageReference = FirebaseStorage.getInstance().getReference().child("dietImageResourceId");
-
-        dietName.setText(getIntent().getStringExtra("wname"));
-        dietDesc.setText(getIntent().getStringExtra("wdes"));
-        dietImage.setImageResource(getIntent().getIntExtra("imag",0));
         update.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -68,5 +70,77 @@ public class DietData extends AppCompatActivity {
                 onBackPressed();
             }
         });
+
+
+        // Retrieve the username from the intent
+        String dietname = getIntent().getStringExtra("dietname");
+
+        // Initialize the database reference to the specific user
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        databaseReference = database.getReference("diets").child(dietname);
+        setDatabaseListener();
+
+        dietName.setText(getIntent().getStringExtra("dietname"));
+        dietDesc.setText(getIntent().getStringExtra("dietdesc"));
+
+
+
+        String getUserImage = getIntent().getStringExtra("dietimage");
+
+        Glide.with(DietData.this)
+                .load(getUserImage) // Load image URL
+                .into(dietImage); // Set the loaded image to the ImageView
     }
+
+    private void setDatabaseListener() {
+        delete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Get the dietname from the intent
+                final String dietname = getIntent().getStringExtra("dietname");
+
+                // Get reference to the "diets" node in the Firebase database
+                DatabaseReference dietsRef = FirebaseDatabase.getInstance().getReference("diets");
+
+                // Query the database to find the specific diet by dietname
+                Query query = dietsRef.orderByChild("dietName").equalTo(dietname);
+                query.addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                        if (dataSnapshot.exists()) {
+                            // Loop through the dataSnapshot and delete the diet with the specified dietname
+                            for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                                snapshot.getRef().removeValue()
+                                        .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                            @Override
+                                            public void onSuccess(Void aVoid) {
+                                                // Diet has been successfully deleted from the Realtime Database
+                                                Toast.makeText(DietData.this, "Record Deleted", Toast.LENGTH_SHORT).show();
+                                                onBackPressed(); // Navigate back after deletion
+                                            }
+                                        })
+                                        .addOnFailureListener(new OnFailureListener() {
+                                            @Override
+                                            public void onFailure(@NonNull Exception e) {
+                                                // Handle errors here
+                                                Toast.makeText(DietData.this, "Failed to delete record: " + e.getMessage(), Toast.LENGTH_SHORT).show();
+                                            }
+                                        });
+                            }
+                        } else {
+                            // Diet with the specified dietname does not exist in the Realtime Database
+                            Toast.makeText(DietData.this, "Diet not found in the database", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+                        // Handle any errors here
+                        Toast.makeText(DietData.this, "Database Error: " + databaseError.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+        });
+    }
+
 }
